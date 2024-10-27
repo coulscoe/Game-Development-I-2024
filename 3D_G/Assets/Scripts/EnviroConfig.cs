6a7c67851ab61a7468143cfb83e1c00ada4d9964
Coen <10960182@uvu.edu>
2024-10-26 23:59:13 -0600

    added checkpoint, scriptable obj, and health UI

---
+++ 3D_G/Assets/Scripts/EnviroConfig.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+[CreateAssetMenu]
+public class EnviroConfig : ScriptableObject
+{
+    
+    public int checkpointAmount = 2;
+    public int harmfulObjs = 1;
+    public int disappearPlat = 1;
+}
