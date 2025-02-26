41137f21e30bcb719027b997b5bafde70926700f
Coen <10960182@uvu.edu>
2025-02-25 19:50:54 -0700

    continued work on game

--- PythonScripts/Auto_Group_Tool.py
+++ PythonScripts/Auto_Group_Tool.py
@@ -1,26 +1,34 @@
 ﻿import maya.cmds as cmds
-def group_tool():
+
+def group_tool(apply_parent_constraint, apply_scale_constraint):
     # gets selection and creates a curve and empty group then parents it together
-    sels= cmds.ls(selection=True)
+    sels = cmds.ls(selection=True)
     for sel in sels:
-        position=cmds.xform(sel, q=True, ws=True, t=True)
-        rotation=cmds.xform(sel, q=True, ws=True, ro=True)
+        position = cmds.xform(sel, q=True, ws=True, t=True)
+        rotation = cmds.xform(sel, q=True, ws=True, ro=True)
         # checks for '_' so it can update the control and the group name
         if '_' in sel:
-            base_name=sel.rpartition('_')[0]
+            base_name = sel.rpartition('_')[0]
         else:
-            base_name=sel
+            base_name = sel
         ctrl_name = f'{base_name}_ctrl'
         group_name = f'{ctrl_name}_grp'
         # creates group and moves and rotates it to the joint
-        obj_group = cmds.group( empty=True, name=group_name)
+        obj_group = cmds.group(empty=True, name=group_name)
         cmds.xform(obj_group, ws=True, t=position)
         cmds.xform(obj_group, ws=True, ro=rotation)
 
         # same but for the circle
-        circle = cmds.circle(name=ctrl_name,radius=.8, normal=[1,0,0])[0]
+        circle = cmds.circle(name=ctrl_name, radius=.8, normal=[1,0,0])[0]
         cmds.xform(circle, ws=True, t=position)
         cmds.xform(circle, ws=True, ro=rotation)
         cmds.parent(circle, obj_group)
-group_tool()
-# requires a string in the format "Name_##_NodeType"
+
+        # Add parent and scale constraints based on function arguments
+        if apply_parent_constraint:
+            cmds.parentConstraint(circle, sel, mo=True)
+        if apply_scale_constraint:
+            cmds.scaleConstraint(circle, sel, mo=True)
+
+# Example usage
+group_tool(apply_parent_constraint=True, apply_scale_constraint=True)
