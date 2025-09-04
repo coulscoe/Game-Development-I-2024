ed42054d1c46304ea3e11f31796cfa2e85bb3824
Coen <10960182@uvu.edu>
2025-09-04 11:14:14 -0600

    created some basic environments, made and fixed a broken FK script

---
+++ PythonScripts/BrokenFKscript.py
@@ -0,0 +1,23 @@
+﻿import maya.cmds as cmds
+
+sels=cmds.ls(sl=True)
+parent_ctrl= sels[0]
+child_ctrl=sels[1]
+
+child_ctrl_grp=cmds.listRelatives(child_ctrl,parent=True)[0]
+
+
+p_constraint1= cmds.parentConstraint(parent_ctrl, child_ctrl_grp, mo=True, skipRotate=['x','y','z'], weight=1) [0]
+p_constraint2= cmds.parentConstraint(parent_ctrl, child_ctrl_grp, mo=True, skipTranslate=['x','y','z'], weight=1) [0]
+cmds.scaleConstraint(parent_ctrl, child_ctrl_grp, offset=[1,1,1], weight=1)
+
+if not cmds.attributeQuery('FollowTranslate', node=child_ctrl, exists=True):
+    cmds.addAttr(child_ctrl, ln='FollowTranslate', at='double', min=0, max=1, dv=1)
+    cmds.setAttr(f'{child_ctrl}.FollowTranslate', e=True, keyable=True)
+
+if not cmds.attributeQuery('FollowRotate', node=child_ctrl, exists=True):
+    cmds.addAttr(child_ctrl, ln='FollowRotate', at='double', min=0, max=1, dv=1)
+    cmds.setAttr(f'{child_ctrl}.FollowRotate', e=True, keyable=True)
+
+cmds.connectAttr(f'{child_ctrl}.FollowTranslate', f'{p_constraint1}.w0', f=True)
+cmds.connectAttr(f'{child_ctrl}.FollowRotate', f'{p_constraint2}.w0', f=True)
